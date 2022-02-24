1f205d33d7b0201226648501a589cf2cd42045cf
imvickykumar999 <imvickykumar999@gmail.com>
2022-02-24 18:37:07 +0530

    welcome

--- app/src/main/java/com/example/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(), "Hey Vicks", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
 
     }
 }
